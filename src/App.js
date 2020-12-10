17d77653a0993d07a523bb809aaf1f4c89d6cb6a
archang <arnoldchang2013@gmail.com>
2020-12-10 17:21:31 -0500

    test table

--- src/App.js
+++ src/App.js
@@ -1,52 +1,181 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components'
+import { useTable, useExpanded } from 'react-table'
 import './App.css';
+import makeData from './makeData'
 // import { Container } from "semantic-ui-react";
-function App() {
-  useEffect(() => {
-    fetch("/scans").then(response =>
-      response.json().then(data => {
-        console.log(data);
-      })
-    );
-  }, []);
 
-  return <div className="App"/>
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+
+function Table({ columns: userColumns, data }) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state: { expanded },
+  } = useTable(
+    {
+      columns: userColumns,
+      data,
+    },
+    useExpanded // Use the useExpanded plugin hook
+  )
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <br />
+      <div>Showing the first 20 results of {rows.length} rows</div>
+      <pre>
+        <code>{JSON.stringify({ expanded: expanded }, null, 2)}</code>
+      </pre>
+    </>
+  )
 }
 
-export default App;
-//   const ListLoading = withListLoading(List);
-//   const [appState, setAppState] = useState({
-//     loading: false,
-//     repos: null,
-//   });
-//
+
+
+
+function App() {
+  const columns = React.useMemo(
+    () => [
+      {
+        // Build our expander column
+        id: 'expander', // Make sure it has an ID
+        Header: ({ getToggleAllRowsExpandedProps, isAllRowsExpanded }) => (
+          <span {...getToggleAllRowsExpandedProps()}>
+            {isAllRowsExpanded ? '👇' : '👉'}
+          </span>
+        ),
+        Cell: ({ row }) =>
+          // Use the row.canExpand and row.getToggleRowExpandedProps prop getter
+          // to build the toggle for expanding a row
+          row.canExpand ? (
+            <span
+              {...row.getToggleRowExpandedProps({
+                style: {
+                  // We can even use the row.depth property
+                  // and paddingLeft to indicate the depth
+                  // of the row
+                  paddingLeft: `${row.depth * 2}rem`,
+                },
+              })}
+            >
+              {row.isExpanded ? '👇' : '👉'}
+            </span>
+          ) : null,
+      },
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  const data = React.useMemo(() => makeData(5, 5, 5), [])
+    console.log(data)
+
+  return (
+    <Styles>
+      <Table columns={columns} data={data} />
+    </Styles>
+  )
+}
+
+export default App
+
+// function App() {
 //   useEffect(() => {
-//     setAppState({ loading: true });
-//     const apiUrl = `scans`;
-//     fetch(apiUrl)
-//       .then((res) => res.json())
-//       .then((repos) => {
-//         setAppState({ loading: false, repos: repos });
-//       });
-//   }, [setAppState]);
-//   return (
-//     <div className='App'>
-//       <div className='container'>
-//         <h1>My Repositories</h1>
-//       </div>
-//       <div className='repo-container'>
-//         <ListLoading isLoading={appState.loading} repos={appState.repos} />
-//       </div>
-//       <footer>
-//         <div className='footer'>
-//           Built{' '}
-//           <span role='img' aria-label='love'>
-//             💚
-//           </span>{' '}
-//           with by Shedrack Akintayo
-//         </div>
-//       </footer>
-//     </div>
-//   );
+//     fetch("/scans").then(response =>
+//       response.json().then(data => {
+//         console.log(data);
+//       })
+//     );
+//   }, []);
+//
+//   return <div className="App"/>
 // }
+//
 // export default App;
